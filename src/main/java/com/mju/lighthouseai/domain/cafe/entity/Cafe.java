525ecad204ff5b421be4e230ebec0727588236ff
pangseon <chaosen0521@gmail.com>
2024-04-07 21:02:14 +0900

    refactor: 유저 엔티티가 필요없어서 해당 컬럼 삭제하였습니다

--- src/main/java/com/mju/lighthouseai/domain/cafe/entity/Cafe.java
+++ src/main/java/com/mju/lighthouseai/domain/cafe/entity/Cafe.java
@@ -1,21 +1,17 @@
 package com.mju.lighthouseai.domain.cafe.entity;
 
-import com.mju.lighthouseai.domain.user.entity.User;
 import com.mju.lighthouseai.global.entity.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
+
 
 @Getter
 @Entity
@@ -38,21 +34,26 @@ public class Cafe extends BaseEntity {
     @Column
     private String menu;
 
-    @ManyToOne
-    @OnDelete(action = OnDeleteAction.CASCADE)
-    @JoinColumn(name = "user_id", nullable = false)
-    private User user;
+    @Column(nullable = false)
+    private String opentime;
+
+    @Column(nullable = false)
+    private String closetime;
 
     @Builder
     public Cafe(
         final String title,
         final String location,
         final int price,
-        final String menu
+        final String menu,
+        final String opentime,
+        final String closetime
     ){
         this.title = title;
         this.location =location;
         this.price = price;
         this.menu = menu;
+        this.opentime = opentime;
+        this.closetime = closetime;
     }
 }
