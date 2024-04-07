8d86a5a0947f8dfa395926821c73b85f7b32eb42
pangseon <chaosen0521@gmail.com>
2024-04-07 21:30:18 +0900

    Feature: BaseEntity에 대한 Config 추가 및 RestTemplateConfig 추가

---
+++ src/main/java/com/mju/lighthouseai/global/config/JpaConfig.java
@@ -0,0 +1,10 @@
+package com.mju.lighthouseai.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+
+}
