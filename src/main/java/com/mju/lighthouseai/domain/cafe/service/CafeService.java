4b4c8326a75ab559bdd7374461d3f1aa28339c2f
pangseon <chaosen0521@gmail.com>
2024-04-12 14:31:12 +0900

    feature: Controller에서 사용할 Update 메서드 생성

--- src/main/java/com/mju/lighthouseai/domain/cafe/service/CafeService.java
+++ src/main/java/com/mju/lighthouseai/domain/cafe/service/CafeService.java
@@ -1,8 +1,9 @@
 package com.mju.lighthouseai.domain.cafe.service;
 
 import com.mju.lighthouseai.domain.cafe.dto.service.request.CafeCreateServiceRequestDto;
+import com.mju.lighthouseai.domain.cafe.dto.service.request.CafeUpdateServiceRequestDto;
 
 public interface CafeService {
     void createCafe(CafeCreateServiceRequestDto requestDto);
-
+    void updateCafe(Long id, CafeUpdateServiceRequestDto requestDto);
 }
