b0d7b937a7aef5ddfe03a7ccea55e23994e7dbd4
Euna Jeong <kysn75@gmail.com>
2024-04-12 18:02:28 +0900

    Feature: MatchCurrentPasswordException 추가 구현 및 UserErrorCode에 MATCH_CURRENT_PASSWORD 추가

--- src/main/java/com/mju/lighthouseai/domain/user/exception/UserErrorCode.java
+++ src/main/java/com/mju/lighthouseai/domain/user/exception/UserErrorCode.java
@@ -13,6 +13,7 @@ public enum UserErrorCode implements ErrorCode {
     ALREADY_EXIST_EMAIL(HttpStatus.BAD_REQUEST, "이미 존재하는 이메일입니다."),
     DUPLICATE_NICKNAME(HttpStatus.BAD_REQUEST, "중복된 닉네임입니다."),
     NOT_MATCH_PASSWORD(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다."),
+    MATCH_CURRENT_PASSWORD(HttpStatus.BAD_REQUEST, "기존 비밀번호입니다."),
     BAD_LOGIN(HttpStatus.BAD_REQUEST, "로그인에 실패하였습니다."),
 
     // 404
