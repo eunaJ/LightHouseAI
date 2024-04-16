2513b7fa9da46de82d3f81e89b5f97fb9fcf3a88
Lee <010tntnf@gmail.com>
2024-04-17 03:26:30 +0900

    feature: 게시판 BoardSaveRequestDto작성

---
+++ src/main/java/com/mju/lighthouseai/domain/board/dto/BoardSaveRequestDto.java
@@ -0,0 +1,35 @@
+package com.mju.lighthouseai.domain.board.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import com.mju.lighthouseai.domain.board.entity.Board;
+
+@Getter
+@NoArgsConstructor
+
+
+public class BoardSaveRequestDto {
+    private String title;
+    private String content;
+    private String author;
+    private String image_url;
+
+    @Builder
+
+    public BoardSaveRequestDto (String title, String content, String author,  String image_url) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+        this.image_url = image_url;
+    }
+    public Board toEntity() {
+        return Board.builder()
+                .title(title)
+                .content(content)
+                .author(author)
+                .image_url(image_url)
+                .build();
+    }
+}
