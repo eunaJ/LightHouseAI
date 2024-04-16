7ea99be7f816a27a3d0d65712281fee0bde9c618
Lee <010tntnf@gmail.com>
2024-04-16 17:59:17 +0900

    feature: 게시판 Board 작성

--- src/main/java/com/mju/lighthouseai/domain/board/entity/Board.java
+++ src/main/java/com/mju/lighthouseai/domain/board/entity/Board.java
@@ -2,15 +2,21 @@ package com.mju.lighthouseai.domain.board.entity;
 
 import com.mju.lighthouseai.domain.user.entity.User;
 import com.mju.lighthouseai.global.entity.BaseEntity;
+
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+
+
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
+
+
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -21,16 +27,20 @@ import org.hibernate.annotations.OnDeleteAction;
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "TB_BOARD")
+
 public class Board extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(length = 500, nullable = false)
     private String title;
 
-    @Column
+    @Column (columnDefinition = "TEXT", nullable = false)
     private String image_url;
+    private String content;
+    private String author;
+
 
     @ManyToOne
     @OnDelete(action = OnDeleteAction.CASCADE)
@@ -42,10 +52,19 @@ public class Board extends BaseEntity {
     public  Board(
         final User user,
         final String title,
-        final String image_url
-    ){
+        final String image_url,
+        final String content,
+        final String author
+    )
+    {
         this.user = user;
         this.title  = title;
         this.image_url = image_url;
+        this.content = content;
+        this.author = author;
+    }
+    public void update(String title, String content) {
+        this.title = title;
+        this.content = content;
     }
 }
