8090207155b5fa8012de4e4c0685772ad1dba540
Euna Jeong <kysn75@gmail.com>
2024-04-25 18:57:48 +0900

    feature: RegionDtoMapper에 toRegionUpdateServiceDto 추가

--- src/main/java/com/mju/lighthouseai/domain/region/mapper/dto/RegionDtoMapper.java
+++ src/main/java/com/mju/lighthouseai/domain/region/mapper/dto/RegionDtoMapper.java
@@ -1,7 +1,9 @@
 package com.mju.lighthouseai.domain.region.mapper.dto;
 
 import com.mju.lighthouseai.domain.region.dto.controller.RegionCreateControllerRequestDto;
+import com.mju.lighthouseai.domain.region.dto.controller.RegionUpdateControllerRequestDto;
 import com.mju.lighthouseai.domain.region.dto.service.request.RegionCreateServiceRequestDto;
+import com.mju.lighthouseai.domain.region.dto.service.request.RegionUpdateServiceRequestDto;
 import org.mapstruct.Mapper;
 
 import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
@@ -10,5 +12,9 @@ import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
 public interface RegionDtoMapper {
     RegionCreateServiceRequestDto toRegionCreateServiceDto(
             RegionCreateControllerRequestDto controllerRequestDto
+    );
+
+    RegionUpdateServiceRequestDto toRegionUpdateServiceDto(
+            RegionUpdateControllerRequestDto controllerRequestDto
     );
 }
