be9b2bba9ecb28cee4dfbcf59feb9399446a2c61
Euna Jeong <kysn75@gmail.com>
2024-04-25 18:58:27 +0900

    feature: RegionService에 updateRegion 추가

--- src/main/java/com/mju/lighthouseai/domain/region/service/RegionService.java
+++ src/main/java/com/mju/lighthouseai/domain/region/service/RegionService.java
@@ -1,8 +1,10 @@
 package com.mju.lighthouseai.domain.region.service;
 
 import com.mju.lighthouseai.domain.region.dto.service.request.RegionCreateServiceRequestDto;
+import com.mju.lighthouseai.domain.region.dto.service.request.RegionUpdateServiceRequestDto;
 import com.mju.lighthouseai.domain.user.entity.User;
 
 public interface RegionService {
     void createRegion(RegionCreateServiceRequestDto requestDto, User user);
+    void updateRegion(Long id, RegionUpdateServiceRequestDto requestDto);
 }
