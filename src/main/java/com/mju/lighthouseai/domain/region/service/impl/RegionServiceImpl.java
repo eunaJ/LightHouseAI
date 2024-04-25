4a1ac7f59a5fa2d3738a151e9a70728edffc9735
Euna Jeong <kysn75@gmail.com>
2024-04-25 18:58:39 +0900

    feature: RegionServiceImpl에 updateRegion, findRegion 추가

--- src/main/java/com/mju/lighthouseai/domain/region/service/impl/RegionServiceImpl.java
+++ src/main/java/com/mju/lighthouseai/domain/region/service/impl/RegionServiceImpl.java
@@ -1,13 +1,17 @@
 package com.mju.lighthouseai.domain.region.service.impl;
 
 import com.mju.lighthouseai.domain.region.dto.service.request.RegionCreateServiceRequestDto;
+import com.mju.lighthouseai.domain.region.dto.service.request.RegionUpdateServiceRequestDto;
 import com.mju.lighthouseai.domain.region.entity.Region;
+import com.mju.lighthouseai.domain.region.exception.NotFoundRegionException;
+import com.mju.lighthouseai.domain.region.exception.RegionErrorCode;
 import com.mju.lighthouseai.domain.region.mapper.service.RegionEntityMapper;
 import com.mju.lighthouseai.domain.region.repository.RegionRepository;
 import com.mju.lighthouseai.domain.region.service.RegionService;
 import com.mju.lighthouseai.domain.user.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
@@ -19,5 +23,15 @@ public class RegionServiceImpl implements RegionService {
     public void createRegion(RegionCreateServiceRequestDto requestDto, User user){
         Region region = regionEntityMapper.toRegion(requestDto, user);
         regionRepository.save(region);
+    }
+
+    @Transactional
+    public void updateRegion(Long id, RegionUpdateServiceRequestDto requestDto){
+        Region region = findRegion(id);
+        region.updateRegion(requestDto.region_name());
+    }
+    private Region findRegion(Long id){
+        return regionRepository.findById(id)
+                .orElseThrow(()-> new NotFoundRegionException(RegionErrorCode.NOT_FOUND_REGION));
     }
 }
