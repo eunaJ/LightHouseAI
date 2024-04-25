8b221be9fcc2c2bed40c139eaec74a318cdf35e3
Euna Jeong <kysn75@gmail.com>
2024-04-26 01:42:42 +0900

    feature: ConstituencyServiceImpl에 updateConstituency, findConstituency 추가

--- src/main/java/com/mju/lighthouseai/domain/constituency/service/impl/ConstituencyServiceImpl.java
+++ src/main/java/com/mju/lighthouseai/domain/constituency/service/impl/ConstituencyServiceImpl.java
@@ -1,13 +1,17 @@
 package com.mju.lighthouseai.domain.constituency.service.impl;
 
 import com.mju.lighthouseai.domain.constituency.dto.service.request.ConstituencyCreateServiceRequestDto;
+import com.mju.lighthouseai.domain.constituency.dto.service.request.ConstituencyUpdateServiceRequestDto;
 import com.mju.lighthouseai.domain.constituency.entity.Constituency;
+import com.mju.lighthouseai.domain.constituency.exception.ConstituencyErrorCode;
+import com.mju.lighthouseai.domain.constituency.exception.NotFoundConstituencyException;
 import com.mju.lighthouseai.domain.constituency.mapper.service.ConstituencyEntityMapper;
 import com.mju.lighthouseai.domain.constituency.repository.ConstituencyRepository;
 import com.mju.lighthouseai.domain.constituency.service.ConstituencyService;
 import com.mju.lighthouseai.domain.user.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
@@ -18,5 +22,16 @@ public class ConstituencyServiceImpl implements ConstituencyService {
     public void createConstituency(ConstituencyCreateServiceRequestDto requestDto, User user){
         Constituency constituency = constituencyEntityMapper.toConstituency(requestDto, user);
         constituencyRepository.save(constituency);
+    }
+
+    @Transactional
+    public void updateConstituency(Long id, ConstituencyUpdateServiceRequestDto requestDto){
+        Constituency constituency = findConstituency(id);
+        constituency.updateConstituency(requestDto.constituency());
+    }
+
+    private Constituency findConstituency(Long id){
+        return constituencyRepository.findById(id)
+                .orElseThrow(()-> new NotFoundConstituencyException(ConstituencyErrorCode.NOT_FOUND_CONSTITUENCY));
     }
 }
