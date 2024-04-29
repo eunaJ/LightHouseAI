022b00b4406e58d3c05f880a4e8f8064c40a6fc0
pangseon <chaosen0521@gmail.com>
2024-04-29 17:40:37 +0900

    feature: 카페 조회를 위한 GetMapping 추가

--- src/main/java/com/mju/lighthouseai/domain/cafe/controller/CafeController.java
+++ src/main/java/com/mju/lighthouseai/domain/cafe/controller/CafeController.java
@@ -12,6 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -52,5 +53,10 @@ public class CafeController {
     ){
         cafeService.deleteCafe(cafeId);
         return ResponseEntity.status(HttpStatus.OK).build();
+    }
+    @GetMapping("/cafes")
+    public ResponseEntity<?> readAllCafes(){
+        return ResponseEntity.status(HttpStatus.OK)
+            .body(cafeService.readAllCafes());
     }
  }
