bc09d5c3c86429a7cfcaccb1f6b4991e4570967e
pangseon <chaosen0521@gmail.com>
2024-04-30 03:22:32 +0900

    refactor: 식당 생성 시 유저 및 지역상세 컬럼 추가

--- src/main/java/com/mju/lighthouseai/domain/restaurant/entity/Restaurant.java
+++ src/main/java/com/mju/lighthouseai/domain/restaurant/entity/Restaurant.java
@@ -1,11 +1,15 @@
 package com.mju.lighthouseai.domain.restaurant.entity;
 
+import com.mju.lighthouseai.domain.constituency.entity.Constituency;
+import com.mju.lighthouseai.domain.user.entity.User;
 import com.mju.lighthouseai.global.entity.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -39,6 +43,13 @@ public class Restaurant extends BaseEntity {
     @Column
     private String closetime;
 
+    @ManyToOne
+    @JoinColumn(name = "user_id",nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "constituency_id",nullable = false)
+    private Constituency constituency;
     @Builder
     public Restaurant(
         final String title,
@@ -46,7 +57,9 @@ public class Restaurant extends BaseEntity {
         final String menu,
         final int price,
         final String opentime,
-        final String closetime
+        final String closetime,
+        final User user,
+        final Constituency constituency
     ) {
         this.title = title;
         this.location = location;
@@ -54,5 +67,7 @@ public class Restaurant extends BaseEntity {
         this.price = price;
         this.opentime = opentime;
         this.closetime = closetime;
+        this.user = user;
+        this.constituency = constituency;
     }
 }
