0e01e75d39df1860b69cab5b61d190a585c955c0
pangseon <chaosen0521@gmail.com>
2024-04-29 17:40:16 +0900

    feature: 카페 조회를 위한 Service 메서드 추가

--- src/main/java/com/mju/lighthouseai/domain/cafe/service/CafeService.java
+++ src/main/java/com/mju/lighthouseai/domain/cafe/service/CafeService.java
@@ -2,10 +2,13 @@ package com.mju.lighthouseai.domain.cafe.service;
 
 import com.mju.lighthouseai.domain.cafe.dto.service.request.CafeCreateServiceRequestDto;
 import com.mju.lighthouseai.domain.cafe.dto.service.request.CafeUpdateServiceRequestDto;
+import com.mju.lighthouseai.domain.cafe.dto.service.response.CafeReadAllServiceResponseDto;
 import com.mju.lighthouseai.domain.user.entity.User;
+import java.util.List;
 
 public interface CafeService {
     void createCafe(CafeCreateServiceRequestDto requestDto, User user);
     void updateCafe(Long id, CafeUpdateServiceRequestDto requestDto);
     void deleteCafe(Long id);
+    List<CafeReadAllServiceResponseDto> readAllCafes();
 }
