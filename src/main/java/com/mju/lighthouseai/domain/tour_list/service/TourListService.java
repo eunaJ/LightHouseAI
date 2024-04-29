2ee1b515111b9c9b4c1f5dd58b05bd6afd1c22f3
pangseon <chaosen0521@gmail.com>
2024-04-30 02:39:36 +0900

    feature: TourListController.java 에서 사용할 createTourList 메서드 생성

---
+++ src/main/java/com/mju/lighthouseai/domain/tour_list/service/TourListService.java
@@ -0,0 +1,15 @@
+package com.mju.lighthouseai.domain.tour_list.service;
+
+import com.mju.lighthouseai.domain.cafe.dto.service.request.CafeCreateServiceRequestDto;
+import com.mju.lighthouseai.domain.cafe.dto.service.request.CafeUpdateServiceRequestDto;
+import com.mju.lighthouseai.domain.cafe.dto.service.response.CafeReadAllServiceResponseDto;
+import com.mju.lighthouseai.domain.tour_list.dto.service.TourListCreateServiceRequestDto;
+import com.mju.lighthouseai.domain.user.entity.User;
+import java.util.List;
+
+public interface TourListService {
+    void createTourList(TourListCreateServiceRequestDto requestDto, User user);
+/*    void updateCafe(Long id, CafeUpdateServiceRequestDto requestDto);
+    void deleteCafe(Long id);
+    List<CafeReadAllServiceResponseDto> readAllCafes();*/
+}
