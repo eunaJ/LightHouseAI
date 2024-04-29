389729389fcc7ed5ba32a36a9e946777c5a37d11
pangseon <chaosen0521@gmail.com>
2024-04-30 04:43:39 +0900

    feature: OtherServiceController 생성 및 postMapping을 이용한 otherService 생성 메서드 구현

---
+++ src/main/java/com/mju/lighthouseai/domain/other_service/controller/OtherServiceController.java
@@ -0,0 +1,38 @@
+package com.mju.lighthouseai.domain.other_service.controller;
+
+import com.mju.lighthouseai.domain.other_service.dto.controller.OtherServiceCreateControllerRequestDto;
+import com.mju.lighthouseai.domain.other_service.dto.service.OtherServiceCreateServiceRequestDto;
+import com.mju.lighthouseai.domain.other_service.mapper.dto.OtherServiceDtoMapper;
+import com.mju.lighthouseai.domain.other_service.service.OtherService;
+import com.mju.lighthouseai.domain.restaurant.dto.controller.RestaurantCreateControllerRequestDto;
+import com.mju.lighthouseai.domain.restaurant.dto.service.RestaurantCreateServiceRequestDto;
+import com.mju.lighthouseai.domain.restaurant.mapper.dto.RestaurantDtoMapper;
+import com.mju.lighthouseai.domain.restaurant.service.RestaurantService;
+import com.mju.lighthouseai.global.security.UserDetailsImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/v1")
+@RestController
+public class OtherServiceController {
+    private final OtherServiceDtoMapper otherServiceDtoMapper;
+    private final OtherService otherService;
+
+    @PostMapping("/other_services/create")
+    public ResponseEntity<?> createRestaurant(
+        @RequestBody OtherServiceCreateControllerRequestDto controllerRequestDto,
+        @AuthenticationPrincipal UserDetailsImpl userDetails
+    ){
+        OtherServiceCreateServiceRequestDto serviceRequestDto =
+            otherServiceDtoMapper.toOtherServiceCreateServiceDto(controllerRequestDto);
+        otherService.createOtherService(serviceRequestDto,userDetails.user());
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+ }
