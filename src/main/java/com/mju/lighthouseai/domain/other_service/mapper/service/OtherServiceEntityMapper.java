d4481a45c8fc93e48673efe4a1659bd865270091
Euna Jeong <kysn75@gmail.com>
2024-04-30 20:27:54 +0900

    feature: OtherServiceEntityMapper에 toOtherServiceReadAllResponseDto 기능 추가 및 관련 메소드들 추가

--- src/main/java/com/mju/lighthouseai/domain/other_service/mapper/service/OtherServiceEntityMapper.java
+++ src/main/java/com/mju/lighthouseai/domain/other_service/mapper/service/OtherServiceEntityMapper.java
@@ -4,13 +4,33 @@ import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
 
 import com.mju.lighthouseai.domain.constituency.entity.Constituency;
 import com.mju.lighthouseai.domain.other_service.dto.service.OtherServiceCreateServiceRequestDto;
+import com.mju.lighthouseai.domain.other_service.dto.service.OtherServiceReadAllServiceResponseDto;
 import com.mju.lighthouseai.domain.other_service.entity.OtherServiceEntity;
+import com.mju.lighthouseai.domain.region.entity.Region;
 import com.mju.lighthouseai.domain.user.entity.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+
+import java.util.List;
 
 @Mapper(componentModel = SPRING)
 public interface OtherServiceEntityMapper {
     @Mapping(source = "constituency",target = "constituency")
     OtherServiceEntity toOtherService(OtherServiceCreateServiceRequestDto requestDto, User user, Constituency constituency);
+
+    default String toUserName(User user){
+        return user.getNickname();
+    }
+    default String toConstituencyName(Constituency constituency){
+        return constituency.getConstituency();
+    }
+    default String toConstituencyRegionName(Region region){
+        return region.getRegion_name();
+    }
+    @Mapping(source = "user",target = "user_name")
+    @Mapping(source = "constituency",target = "constituency_name")
+    @Mapping(source = "constituency.region",target = "region_name")
+    OtherServiceReadAllServiceResponseDto toOtherServiceReadResponseDto(OtherServiceEntity otherServiceEntity);
+
+    List<OtherServiceReadAllServiceResponseDto> toOtherServiceReadAllResponseDto(List<OtherServiceEntity> otherServiceEntityList);
 }
