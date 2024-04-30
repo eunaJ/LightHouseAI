5dec459336816681c4ce0ece5118ad2863582512
Euna Jeong <kysn75@gmail.com>
2024-04-30 17:20:51 +0900

    feature: RestaurantDtoMapper, RestaurantEntityMapper 추가

--- src/main/java/com/mju/lighthouseai/domain/restaurant/mapper/dto/RestaurantDtoMapper.java
+++ src/main/java/com/mju/lighthouseai/domain/restaurant/mapper/dto/RestaurantDtoMapper.java
@@ -3,9 +3,9 @@ package com.mju.lighthouseai.domain.restaurant.mapper.dto;
 import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
 
 import com.mju.lighthouseai.domain.restaurant.dto.controller.RestaurantUpdateControllerRequestDto;
-import com.mju.lighthouseai.domain.restaurant.dto.service.RestaurantCreateServiceRequestDto;
+import com.mju.lighthouseai.domain.restaurant.dto.service.request.RestaurantCreateServiceRequestDto;
 import com.mju.lighthouseai.domain.restaurant.dto.controller.RestaurantCreateControllerRequestDto;
-import com.mju.lighthouseai.domain.restaurant.dto.service.RestaurantUpdateServiceRequestDto;
+import com.mju.lighthouseai.domain.restaurant.dto.service.request.RestaurantUpdateServiceRequestDto;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = SPRING)
