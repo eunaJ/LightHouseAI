f1e7ef2cf313edbd92a6aae41208df3e9a63f3fb
Euna Jeong <kysn75@gmail.com>
2024-04-30 20:08:13 +0900

    feature: OtherService에 updateOtherService 기능 추가

--- src/main/java/com/mju/lighthouseai/domain/other_service/service/OtherService.java
+++ src/main/java/com/mju/lighthouseai/domain/other_service/service/OtherService.java
@@ -1,8 +1,11 @@
 package com.mju.lighthouseai.domain.other_service.service;
 
 import com.mju.lighthouseai.domain.other_service.dto.service.OtherServiceCreateServiceRequestDto;
+import com.mju.lighthouseai.domain.other_service.dto.service.OtherServiceUpdateServiceRequestDto;
 import com.mju.lighthouseai.domain.user.entity.User;
 
 public interface OtherService {
     void createOtherService(OtherServiceCreateServiceRequestDto requestDto, User user);
+
+    void updateOtherService(Long id, OtherServiceUpdateServiceRequestDto requestDto, User user);
 }
