38612658013be734b2612f28eed8f719d6bf6db5
pangseon <chaosen0521@gmail.com>
2024-04-30 15:09:35 +0900

    refactor: tourList 업데이트시 controller -> service dto 변환 mapper 메서드 추가

--- src/main/java/com/mju/lighthouseai/domain/tour_list/mapper/dto/TourListDtoMapper.java
+++ src/main/java/com/mju/lighthouseai/domain/tour_list/mapper/dto/TourListDtoMapper.java
@@ -6,13 +6,18 @@ import com.mju.lighthouseai.domain.cafe.dto.controller.CafeCreateControllerReque
 import com.mju.lighthouseai.domain.cafe.dto.controller.CafeUpdateControllerRequestDto;
 import com.mju.lighthouseai.domain.cafe.dto.service.request.CafeUpdateServiceRequestDto;
 import com.mju.lighthouseai.domain.tour_list.dto.controller.TourListCreateControllerRequestDto;
+import com.mju.lighthouseai.domain.tour_list.dto.controller.TourListUpdateControllerRequestDto;
 import com.mju.lighthouseai.domain.tour_list.dto.service.TourListCreateServiceRequestDto;
+import com.mju.lighthouseai.domain.tour_list.dto.service.TourListUpdateServiceRequestDto;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = SPRING)
 public interface TourListDtoMapper {
     TourListCreateServiceRequestDto toTourListCreateServiceDto(
         TourListCreateControllerRequestDto controllerRequestDto
+    );
+    TourListUpdateServiceRequestDto toTourListUpdateServiceDto(
+        TourListUpdateControllerRequestDto controllerRequestDto
     );
 
 }
