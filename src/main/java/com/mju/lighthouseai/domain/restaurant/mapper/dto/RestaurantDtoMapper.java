43147b96e84427d6ee80be44379014c53d6a30bf
Euna Jeong <kysn75@gmail.com>
2024-04-30 16:34:15 +0900

    feature: RestaurantDtoMapper에 toRestaurantUpdateServiceDto 기능 추가

--- src/main/java/com/mju/lighthouseai/domain/restaurant/mapper/dto/RestaurantDtoMapper.java
+++ src/main/java/com/mju/lighthouseai/domain/restaurant/mapper/dto/RestaurantDtoMapper.java
@@ -2,8 +2,10 @@ package com.mju.lighthouseai.domain.restaurant.mapper.dto;
 
 import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
 
+import com.mju.lighthouseai.domain.restaurant.dto.controller.RestaurantUpdateControllerRequestDto;
 import com.mju.lighthouseai.domain.restaurant.dto.service.RestaurantCreateServiceRequestDto;
 import com.mju.lighthouseai.domain.restaurant.dto.controller.RestaurantCreateControllerRequestDto;
+import com.mju.lighthouseai.domain.restaurant.dto.service.RestaurantUpdateServiceRequestDto;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = SPRING)
@@ -12,4 +14,7 @@ public interface RestaurantDtoMapper {
         RestaurantCreateControllerRequestDto controllerRequestDto
     );
 
+    RestaurantUpdateServiceRequestDto toRestaurantUpdateServiceDto(
+            RestaurantUpdateControllerRequestDto controllerRequestDto
+    );
 }
