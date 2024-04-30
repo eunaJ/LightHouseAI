0f21c992bbbf76d3e2bdfafa8a3c63a8646ec9d7
Euna Jeong <kysn75@gmail.com>
2024-04-30 19:40:26 +0900

    refactor: OtherServiceEntity에 opentime, closetime 추가

--- src/main/java/com/mju/lighthouseai/domain/other_service/entity/OtherServiceEntity.java
+++ src/main/java/com/mju/lighthouseai/domain/other_service/entity/OtherServiceEntity.java
@@ -37,6 +37,12 @@ public class OtherServiceEntity extends BaseEntity
     @Column
     private int price;
 
+    @Column
+    private String opentime;
+
+    @Column
+    private String closetime;
+
     @ManyToOne
     @JoinColumn(name = "user_id",nullable = false)
     private User user;
@@ -48,18 +54,21 @@ public class OtherServiceEntity extends BaseEntity
 
     @Builder
     public OtherServiceEntity(
-        final String title,
-        final String location,
-        final int price,
-        final User user,
-        final Constituency constituency
+            final String title,
+            final String location,
+            final int price,
+            final String opentime,
+            final String closetime,
+            final User user,
+            final Constituency constituency
     ) {
         this.title = title;
         this.location = location;
         this.price = price;
+        this.opentime = opentime;
+        this.closetime = closetime;
         this.user = user;
         this.constituency = constituency;
     }
-
 }
 
