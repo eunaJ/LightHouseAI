664e66c1afc3c3ccade398af0821ed46acff389c
pangseon <chaosen0521@gmail.com>
2024-04-30 23:21:11 +0900

    feature: ShoppingMall 전체 조회 및 단건 조회를 위한 메서드 추가

--- src/main/java/com/mju/lighthouseai/domain/shoppingmall/service/ShoppingMallService.java
+++ src/main/java/com/mju/lighthouseai/domain/shoppingmall/service/ShoppingMallService.java
@@ -1,10 +1,14 @@
 package com.mju.lighthouseai.domain.shoppingmall.service;
 
-import com.mju.lighthouseai.domain.shoppingmall.dto.service.ShoppingMallCreateServiceRequestDto;
-import com.mju.lighthouseai.domain.shoppingmall.dto.service.ShoppingMallUpdateServiceRequestDto;
+import com.mju.lighthouseai.domain.shoppingmall.dto.service.request.ShoppingMallCreateServiceRequestDto;
+import com.mju.lighthouseai.domain.shoppingmall.dto.service.request.ShoppingMallUpdateServiceRequestDto;
+import com.mju.lighthouseai.domain.shoppingmall.dto.service.response.ShoppingMallReadAllServiceResponseDto;
 import com.mju.lighthouseai.domain.user.entity.User;
+import java.util.List;
 
 public interface ShoppingMallService {
     void createShoppingMall(ShoppingMallCreateServiceRequestDto requestDto, User user);
     void updateShoppingMall(Long id, ShoppingMallUpdateServiceRequestDto requestDto,User user);
+    ShoppingMallReadAllServiceResponseDto readShoppingMall(Long id);
+    List<ShoppingMallReadAllServiceResponseDto> readAllShoppingMall();
 }
