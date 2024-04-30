1c607a72d1c162d8d080fa0402bd63280fb11472
Euna Jeong <kysn75@gmail.com>
2024-04-30 20:10:44 +0900

    feature: OtherServiceEntity에 updateOtherService 추가

--- src/main/java/com/mju/lighthouseai/domain/other_service/entity/OtherServiceEntity.java
+++ src/main/java/com/mju/lighthouseai/domain/other_service/entity/OtherServiceEntity.java
@@ -70,5 +70,16 @@ public class OtherServiceEntity extends BaseEntity
         this.user = user;
         this.constituency = constituency;
     }
+
+    public void updateOtherService(
+            String title, String location, int price,
+            String opentime, String closetime, Constituency constituency){
+        this.title = title;
+        this.location = location;
+        this.price = price;
+        this.opentime = opentime;
+        this.closetime = closetime;
+        this.constituency = constituency;
+    }
 }
 
