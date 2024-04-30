b4df13ce33203a5c5d5b3cdfbdf5162edf81cab5
pangseon <chaosen0521@gmail.com>
2024-04-30 20:37:15 +0900

    feature: ShoppingMall 업데이트 메서드 추가

--- src/main/java/com/mju/lighthouseai/domain/shoppingmall/service/ShoppingMallService.java
+++ src/main/java/com/mju/lighthouseai/domain/shoppingmall/service/ShoppingMallService.java
@@ -1,9 +1,10 @@
 package com.mju.lighthouseai.domain.shoppingmall.service;
 
 import com.mju.lighthouseai.domain.shoppingmall.dto.service.ShoppingMallCreateServiceRequestDto;
+import com.mju.lighthouseai.domain.shoppingmall.dto.service.ShoppingMallUpdateServiceRequestDto;
 import com.mju.lighthouseai.domain.user.entity.User;
 
 public interface ShoppingMallService {
     void createShoppingMall(ShoppingMallCreateServiceRequestDto requestDto, User user);
-
+    void updateShoppingMall(Long id, ShoppingMallUpdateServiceRequestDto requestDto,User user);
 }
