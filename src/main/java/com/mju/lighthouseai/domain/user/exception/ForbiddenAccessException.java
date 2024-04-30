1f497e46ff17bca87121db4b77161c06d26b9d70
Euna Jeong <kysn75@gmail.com>
2024-04-30 11:39:12 +0900

    refactor: ForbiddenAccessTestException -> ForbiddenAccessException 으로 수정

--- src/main/java/com/mju/lighthouseai/domain/user/exception/ForbiddenAccessException.java
+++ src/main/java/com/mju/lighthouseai/domain/user/exception/ForbiddenAccessException.java
@@ -4,9 +4,9 @@ package com.mju.lighthouseai.domain.user.exception;
 import com.mju.lighthouseai.global.exception.CustomException;
 import com.mju.lighthouseai.global.exception.ErrorCode;
 
-public class ForbiddenAccessTestException extends CustomException {
+public class ForbiddenAccessException extends CustomException {
 
-    public ForbiddenAccessTestException(final ErrorCode errorCode) {
+    public ForbiddenAccessException(final ErrorCode errorCode) {
         super(errorCode);
     }
 }
