de52492366580dad59f1fc28936c164bc3808a8d
Euna Jeong <kysn75@gmail.com>
2024-04-30 18:16:25 +0900

    refactor: createRestaurant -> createOtherService로 메서드 이름 수정

--- src/main/java/com/mju/lighthouseai/domain/other_service/controller/OtherServiceController.java
+++ src/main/java/com/mju/lighthouseai/domain/other_service/controller/OtherServiceController.java
@@ -9,20 +9,17 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/otherServices")
 @RestController
 public class OtherServiceController {
     private final OtherServiceDtoMapper otherServiceDtoMapper;
     private final OtherService otherService;
 
-    @PostMapping("/other_services/create")
-    public ResponseEntity<?> createRestaurant(
+    @PostMapping("/create")
+    public ResponseEntity<?> createOtherservice(
         @RequestBody OtherServiceCreateControllerRequestDto controllerRequestDto,
         @AuthenticationPrincipal UserDetailsImpl userDetails
     ){
