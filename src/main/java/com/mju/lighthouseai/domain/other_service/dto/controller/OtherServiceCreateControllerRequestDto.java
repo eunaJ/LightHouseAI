652a1a1642caf48a26c530724481a46b354e59a6
Euna Jeong <kysn75@gmail.com>
2024-04-30 19:42:26 +0900

    refactor: OtherServiceCreateServiceRequestDto, OtherServiceCreateControllerRequestDto에 opentime, closetime 추가

--- src/main/java/com/mju/lighthouseai/domain/other_service/dto/controller/OtherServiceCreateControllerRequestDto.java
+++ src/main/java/com/mju/lighthouseai/domain/other_service/dto/controller/OtherServiceCreateControllerRequestDto.java
@@ -1,10 +1,12 @@
 package com.mju.lighthouseai.domain.other_service.dto.controller;
 
 public record OtherServiceCreateControllerRequestDto(
-    String title,
-    String location,
-    Integer price,
-    String constituency_name
+        String title,
+        String location,
+        int price,
+        String opentime,
+        String closetime,
+        String constituency_name
 
 ) {
 
