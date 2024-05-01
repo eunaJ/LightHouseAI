9d996ba5d45ab753ee5cb468f286d326e567f168
Euna Jeong <kysn75@gmail.com>
2024-05-01 19:08:34 +0900

    refactor: 쿠키 생성 시 만료 시간 refreshTokenExpirationPeriod으로 변경

--- src/main/java/com/mju/lighthouseai/global/jwt/JwtUtil.java
+++ src/main/java/com/mju/lighthouseai/global/jwt/JwtUtil.java
@@ -130,7 +130,7 @@ public class JwtUtil {
 
     public Cookie createCookie(String key, String value) {
         Cookie cookie = new Cookie(key, value);
-        cookie.setMaxAge(24*60*60);
+        cookie.setMaxAge(Math.toIntExact(refreshTokenExpirationPeriod));
         cookie.setHttpOnly(true);
         return cookie;
     }
