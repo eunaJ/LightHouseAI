cd327e1a1d756975f1eb2e8e2fd6474c65c6b1d1
Euna Jeong <kysn75@gmail.com>
2024-05-02 18:32:58 +0900

    refactor: refreshToken index 가능하도록 추가

--- src/main/java/com/mju/lighthouseai/global/jwt/RefreshToken.java
+++ src/main/java/com/mju/lighthouseai/global/jwt/RefreshToken.java
@@ -3,13 +3,18 @@ package com.mju.lighthouseai.global.jwt;
 import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.RedisKeyValueAdapter;
+import org.springframework.data.redis.core.index.Indexed;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @Getter
+@EnableRedisRepositories(enableKeyspaceEvents = RedisKeyValueAdapter.EnableKeyspaceEvents.ON_STARTUP)
 @RedisHash(value = "RefreshToken")
 public class RefreshToken {
     @Id
     private final Long id;
 
+    @Indexed
     private final String refreshToken;
 
     //@Value("${spring.jwt.refresh.expiration-period}") 참고용
