603ab445e345c3606ccf0d50f6dcbc42ef4370df
Euna Jeong <kysn75@gmail.com>
2024-05-05 19:22:53 +0900

    feature: TravelVisitorCafeDtoMapper, TravelVisitorCafeEntityMapper toTravelVisitorCafeCreateServiceDto 기능 추가

---
+++ src/main/java/com/mju/lighthouseai/domain/travel_visitor_cafe/mapper/dto/TravelVisitorCafeDtoMapper.java
@@ -0,0 +1,14 @@
+package com.mju.lighthouseai.domain.travel_visitor_cafe.mapper.dto;
+
+import com.mju.lighthouseai.domain.travel_visitor_cafe.dto.controller.TravelVisitorCafeCreateControllerRequestDto;
+import com.mju.lighthouseai.domain.travel_visitor_cafe.dto.service.TravelVisitorCafeCreateServiceRequestDto;
+import org.mapstruct.Mapper;
+
+import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
+
+@Mapper(componentModel = SPRING)
+public interface TravelVisitorCafeDtoMapper {
+    TravelVisitorCafeCreateServiceRequestDto toTravelVisitorCafeCreateServiceDto(
+            TravelVisitorCafeCreateControllerRequestDto controllerRequestDto
+    );
+}
---
+++ src/main/java/com/mju/lighthouseai/domain/travel_visitor_cafe/mapper/service/TravelVisitorCafeEntityMapper.java
@@ -0,0 +1,24 @@
+package com.mju.lighthouseai.domain.travel_visitor_cafe.mapper.service;
+
+import com.mju.lighthouseai.domain.cafe.entity.Cafe;
+import com.mju.lighthouseai.domain.travel_visitor_cafe.dto.service.TravelVisitorCafeCreateServiceRequestDto;
+import com.mju.lighthouseai.domain.travel_visitor_cafe.entity.TravelVisitorCafe;
+import com.mju.lighthouseai.domain.user.entity.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
+
+@Mapper(componentModel = SPRING)
+public interface TravelVisitorCafeEntityMapper {
+    @Mapping(source = "requestDto.opentime", target = "opentime")
+    @Mapping(source = "requestDto.closetime", target = "closetime")
+    @Mapping(source = "requestDto.location", target = "location")
+    @Mapping(source = "cafe", target = "cafe")
+    TravelVisitorCafe toTravelVisitorCafe(TravelVisitorCafeCreateServiceRequestDto requestDto,
+                                          User user, Cafe cafe, String image_url);
+
+    default String toCafeTitle(Cafe cafe){
+        return cafe.getTitle();
+    }
+}
