0dbffffa83bbf51b59e7373f3e0aa411563059fb
Euna Jeong <kysn75@gmail.com>
2024-05-05 23:52:16 +0900

    feature: TravelVisitorRestaurant 업데이트 시 dto mapper 추가

--- src/main/java/com/mju/lighthouseai/domain/travel_visitor_restaurant/mapper/dto/TravelVisitorRestaurantDtoMapper.java
+++ src/main/java/com/mju/lighthouseai/domain/travel_visitor_restaurant/mapper/dto/TravelVisitorRestaurantDtoMapper.java
@@ -1,7 +1,9 @@
 package com.mju.lighthouseai.domain.travel_visitor_restaurant.mapper.dto;
 
 import com.mju.lighthouseai.domain.travel_visitor_restaurant.dto.controller.TravelVisitorRestaurantCreateControllerRequestDto;
+import com.mju.lighthouseai.domain.travel_visitor_restaurant.dto.controller.TravelVisitorRestaurantUpdateControllerRequestDto;
 import com.mju.lighthouseai.domain.travel_visitor_restaurant.dto.service.request.TravelVisitorRestaurantCreateServiceRequestDto;
+import com.mju.lighthouseai.domain.travel_visitor_restaurant.dto.service.request.TravelVisitorRestaurantUpdateServiceRequestDto;
 import org.mapstruct.Mapper;
 
 import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
@@ -10,5 +12,9 @@ import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
 public interface TravelVisitorRestaurantDtoMapper {
     TravelVisitorRestaurantCreateServiceRequestDto toTravelVisitorRestaurantCreateServiceDto(
             TravelVisitorRestaurantCreateControllerRequestDto controllerRequestDto
+    );
+
+    TravelVisitorRestaurantUpdateServiceRequestDto toTravelVisitorRestaurantUpdateServiceDto(
+            TravelVisitorRestaurantUpdateControllerRequestDto controllerRequestDto
     );
 }
