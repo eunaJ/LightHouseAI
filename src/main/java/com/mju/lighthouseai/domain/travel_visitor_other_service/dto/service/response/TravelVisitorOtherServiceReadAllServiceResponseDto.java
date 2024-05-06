fd15d029a09cfd9c49d51b5982bab93c45920889
Euna Jeong <kysn75@gmail.com>
2024-05-06 13:46:56 +0900

    feature: TravelVisitorOtherServicee 단건 조회 기능 위한 dto 추가

---
+++ src/main/java/com/mju/lighthouseai/domain/travel_visitor_other_service/dto/service/response/TravelVisitorOtherServiceReadAllServiceResponseDto.java
@@ -0,0 +1,13 @@
+package com.mju.lighthouseai.domain.travel_visitor_other_service.dto.service.response;
+
+public record TravelVisitorOtherServiceReadAllServiceResponseDto(
+        Long id,
+        int price,
+        String opentime,
+        String closetime,
+        String location,
+        String nickname,
+        String otherService_title,
+        String image_url
+) {
+}
