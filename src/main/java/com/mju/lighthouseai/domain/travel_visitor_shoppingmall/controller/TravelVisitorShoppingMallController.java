d270443c947e16668a2401792d2b191b6d26d3eb
Euna Jeong <kysn75@gmail.com>
2024-05-06 12:50:52 +0900

    refactor: request mapping travelVisitorShoppingMalls 로 수정

--- src/main/java/com/mju/lighthouseai/domain/travel_visitor_shoppingmall/controller/TravelVisitorShoppingMallController.java
+++ src/main/java/com/mju/lighthouseai/domain/travel_visitor_shoppingmall/controller/TravelVisitorShoppingMallController.java
@@ -17,7 +17,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/travelvisitorshoppingmalls")
+@RequestMapping("/api/v1/travelVisitorShoppingMalls")
 @RestController
 public class TravelVisitorShoppingMallController {
     private final TravelVisitorShoppingMallDtoMapper travelVisitorShoppingMallDtoMapper;
