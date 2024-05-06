9b53c4dfb386db1570c834dd3a4825d110c28cc0
Euna Jeong <kysn75@gmail.com>
2024-05-06 12:44:35 +0900

    feature: TravelVisitorTourList 단건 조회 위한 response to 추가

---
+++ src/main/java/com/mju/lighthouseai/domain/travel_visitor_tour_list/dto/service/response/TravelVisitorTourListReadAllServiceResponseDto.java
@@ -0,0 +1,13 @@
+package com.mju.lighthouseai.domain.travel_visitor_tour_list.dto.service.response;
+
+public record TravelVisitorTourListReadAllServiceResponseDto(
+        Long id,
+        int price,
+        String opentime,
+        String closetime,
+        String location,
+        String nickname,
+        String tourList_title,
+        String image_url
+) {
+}
