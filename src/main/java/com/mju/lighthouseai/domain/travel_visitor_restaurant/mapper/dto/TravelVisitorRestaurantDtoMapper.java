3ecd4d366c5921a9651bf3f25a0cc5f5ac195e14
pangseon <chaosen0521@gmail.com>
2024-05-15 15:13:15 +0900

    feature: List 형태로 dto 전달

--- src/main/java/com/mju/lighthouseai/domain/travel_visitor_restaurant/mapper/dto/TravelVisitorRestaurantDtoMapper.java
+++ src/main/java/com/mju/lighthouseai/domain/travel_visitor_restaurant/mapper/dto/TravelVisitorRestaurantDtoMapper.java
@@ -4,12 +4,17 @@ import com.mju.lighthouseai.domain.travel_visitor_restaurant.dto.controller.Trav
 import com.mju.lighthouseai.domain.travel_visitor_restaurant.dto.controller.TravelVisitorRestaurantUpdateControllerRequestDto;
 import com.mju.lighthouseai.domain.travel_visitor_restaurant.dto.service.request.TravelVisitorRestaurantCreateServiceRequestDto;
 import com.mju.lighthouseai.domain.travel_visitor_restaurant.dto.service.request.TravelVisitorRestaurantUpdateServiceRequestDto;
+import com.mju.lighthouseai.domain.travel_visitor_restaurant.entity.TravelVisitorRestaurant;
+import java.util.List;
 import org.mapstruct.Mapper;
 
 import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
 
 @Mapper(componentModel = SPRING)
 public interface TravelVisitorRestaurantDtoMapper {
+    List<TravelVisitorRestaurantCreateServiceRequestDto> toTravelVisitorRestaurantCreateServiceDtos(
+        List<TravelVisitorRestaurantCreateControllerRequestDto>createControllerRequestDto
+    );
     TravelVisitorRestaurantCreateServiceRequestDto toTravelVisitorRestaurantCreateServiceDto(
             TravelVisitorRestaurantCreateControllerRequestDto controllerRequestDto
     );
