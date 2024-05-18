8e69854ff4988498498f483c08fa58859b462e85
pangseon <chaosen0521@gmail.com>
2024-05-18 14:56:55 +0900

    refactor: 여행지지 조회 시 constituency 추가

--- src/main/java/com/mju/lighthouseai/domain/travel/dto/service/response/TravelReadAllServiceResponseDto.java
+++ src/main/java/com/mju/lighthouseai/domain/travel/dto/service/response/TravelReadAllServiceResponseDto.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 @Builder
 public record TravelReadAllServiceResponseDto(
     Long id,
+    String constituency_name,
     String title,
     Integer travel_expense,
     Byte serving,
