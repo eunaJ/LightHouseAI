805ba4911161fd5e0f14dd9a7885575caf0daa17
pangseon <chaosen0521@gmail.com>
2024-05-19 22:09:23 +0900

    refactor: dto 및 변경사항 추가

--- src/main/java/com/mju/lighthouseai/domain/travel/dto/service/request/TravelUpdateServiceRequestDto.java
+++ src/main/java/com/mju/lighthouseai/domain/travel/dto/service/request/TravelUpdateServiceRequestDto.java
@@ -2,7 +2,6 @@ package com.mju.lighthouseai.domain.travel.dto.service.request;
 
 public record TravelUpdateServiceRequestDto(
     String title,
-    Integer travel_expense,
     Byte serving,
     Byte star,
     Boolean imageChange
