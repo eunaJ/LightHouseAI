9401e189d9a67c0f40eb28ca27b9fde5c31e465d
pangseon <chaosen0521@gmail.com>
2024-05-19 15:41:32 +0900

    refactor: 여행지_방문지_식당 content 추가

--- src/main/java/com/mju/lighthouseai/domain/travel_visitor_restaurant/dto/controller/TravelVisitorRestaurantUpdateControllerRequestDto.java
+++ src/main/java/com/mju/lighthouseai/domain/travel_visitor_restaurant/dto/controller/TravelVisitorRestaurantUpdateControllerRequestDto.java
@@ -3,6 +3,7 @@ package com.mju.lighthouseai.domain.travel_visitor_restaurant.dto.controller;
 public record TravelVisitorRestaurantUpdateControllerRequestDto(
         String menu,
         Integer price,
+        String content,
         String opentime,
         String closetime,
         String location,
