bd3914c468e1cc6eaeba1a0dd4e62148c67de67d
pangseon <chaosen0521@gmail.com>
2024-05-19 16:06:11 +0900

    refactor: content 추가

--- src/main/java/com/mju/lighthouseai/domain/travel_visitor_other_service/dto/controller/TravelVisitorOtherServiceCreateControllerRequestDto.java
+++ src/main/java/com/mju/lighthouseai/domain/travel_visitor_other_service/dto/controller/TravelVisitorOtherServiceCreateControllerRequestDto.java
@@ -1,7 +1,8 @@
 package com.mju.lighthouseai.domain.travel_visitor_other_service.dto.controller;
 
 public record TravelVisitorOtherServiceCreateControllerRequestDto(
-        int price,
+        Integer price,
+        String content,
         String opentime,
         String closetime,
         String location,
