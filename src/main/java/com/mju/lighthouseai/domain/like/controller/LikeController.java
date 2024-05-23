6f4f765d32fb5437157b084caafb7cc070e0e9bd
Euna Jeong <kysn75@gmail.com>
2024-05-23 12:00:49 +0900

    refactor: 좋아요 추가, 삭제, getall 기능 수정

---
+++ src/main/java/com/mju/lighthouseai/domain/like/controller/LikeController.java
@@ -0,0 +1,48 @@
+package com.mju.lighthouseai.domain.like.controller;
+
+import com.mju.lighthouseai.global.security.UserDetailsImpl;
+import com.mju.lighthouseai.domain.like.service.LikeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/v1")
+@RestController
+public class LikeController {
+
+    private final LikeService likeService;
+
+    @PostMapping("/boards/{boardId}/likes")
+    public ResponseEntity<?> addBoardLike(
+            @PathVariable Long boardId,
+            @AuthenticationPrincipal UserDetailsImpl userDetails
+    ){
+        likeService.addLike(boardId, userDetails.user());
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+    @DeleteMapping("/boards/{boardId}/likes")
+    public ResponseEntity<?> deleteBoardLike(
+            @PathVariable Long boardId,
+            @AuthenticationPrincipal UserDetailsImpl userDetails
+    ){
+        likeService.deleteLike(boardId, userDetails.user());
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+    @GetMapping("/boards/{boardId}/likes")
+    public ResponseEntity<Long> readAllLikes(
+            @PathVariable Long boardId
+    ){
+        return ResponseEntity.status(HttpStatus.OK).
+                body(likeService.getAllLikes(boardId));
+    }
+}
---
+++ src/main/java/com/mju/lighthouseai/domain/like/service/LikeService.java
@@ -0,0 +1,9 @@
+package com.mju.lighthouseai.domain.like.service;
+
+import com.mju.lighthouseai.domain.user.entity.User;
+
+public interface LikeService {
+    void addLike (Long boardId, User user);
+    void deleteLike(Long boardId, User user);
+    Long getAllLikes (Long boardId);
+}
