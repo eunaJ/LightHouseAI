6f5b9c0790134abf02d89a33edc878e060915d27
choi wonjun <serty1234@mju.ac.kr>
2024-05-25 03:38:36 +0900

    ai 비용, 인원수 기능 추가

--- src/main/python/Chatbot.py
+++ src/main/python/Chatbot.py
@@ -58,9 +58,6 @@ def cafe_cheap(location):
     #travel_id를 10개 추출
     travel_id = df_travel_visitor_cafe['travel_id'].tolist()
 
-    #travel_id를 10개 추출
-    travel_id = travel_id[:10]
-
     return travel_id
 
 def restaurant_cheap(location):
@@ -94,9 +91,6 @@ def restaurant_cheap(location):
     #travel_id를 추출
     travel_id = df_travel_visitor_restaurant['travel_id'].tolist()
 
-    #travel_id를 10개 추출
-    travel_id = travel_id[:10]
-
     return travel_id
 
 def shopping_cheap(location):
@@ -127,8 +121,6 @@ def shopping_cheap(location):
     #travel_id를 추출
     travel_id = df_travel_visitor_shopping['travel_id'].tolist()
 
-    #travel_id를 10개 추출
-    travel_id = travel_id[:10]
 
     return travel_id
 
@@ -160,9 +152,6 @@ def tourist_cheap(location):
     #travel_id를 추출
     travel_id = df_travel_visitor_tourist['travel_id'].tolist()
 
-    #travel_id를 10개 추출
-    travel_id = travel_id[:10]
-
     return travel_id
 
 def other_cheap(location):
@@ -192,9 +181,6 @@ def other_cheap(location):
 
     #travel_id를 추출
     travel_id = df_travel_visitor_other['travel_id'].tolist()
-
-    #travel_id를 10개 추출
-    travel_id = travel_id[:10]
 
     return travel_id
 
@@ -419,19 +405,19 @@ def find_using_id(travel_id):
     dbconn.execute(f"SELECT * FROM TB_CAFE WHERE id = {df_travel_visitor_cafe['cafe_id'].values[0]}")
     
     result = dbconn.fetchall()
+
+    print(result)
     
     df_cafe = pd.DataFrame(result)
     df_cafe.columns = [
         'id',
         'createdAt',
         'modifiedAt',
-        'image_url',
+        'closetime',
         'location',
-        'menu',
         'opentime',
-        'price',
-        'star',
         'title',
+        'constituency_id',
         'user_id',
     ]
     
@@ -467,6 +453,28 @@ def find_using_id(travel_id):
     #id를 TRAVEL_RESTAURANT_ID로 변경
     df_travel_visitor_restaurant = df_travel_visitor_restaurant.rename(columns={'id': 'travel_restaurant_id'})
 
+    #TB_RESTAURANT에서 restaurant_id가 restaurant_id인 데이터를 추출해 title을 추출하여 df_travel_visitor_restaurant에 추가
+    dbconn = conn.cursor()
+    dbconn.execute(f"SELECT * FROM TB_RESTAURANT WHERE id = {df_travel_visitor_restaurant['restaurant_id'].values[0]}")
+
+    result = dbconn.fetchall()
+
+    df_restaurant = pd.DataFrame(result)
+    df_restaurant.columns = [
+        'id',
+        'createdAt',
+        'modifiedAt',
+        'closetime',
+        'location',
+        'opentime',
+        'title',
+        'constituency_id',
+        'user_id',
+    ]
+
+    #title만 df_travel_visitor_restaurant에 추가
+    df_travel_visitor_restaurant['title'] = df_restaurant['title'].values[0]
+
     #TB_TRAVEL_VISITOR_SHOPPINGMALL에서 travel_id가 travel_id인 데이터를 추출
     dbconn = conn.cursor()
     dbconn.execute(f"SELECT * FROM TB_TRAVEL_VISITOR_SHOPPINGMALL WHERE travel_id = {travel_id}")
@@ -494,6 +502,28 @@ def find_using_id(travel_id):
 
     #id를 TRAVEL_SHOPPING_ID로 변경
     df_travel_visitor_shopping = df_travel_visitor_shopping.rename(columns={'id': 'travel_shoppingmall_id'})
+
+    #TB_SHOPPINGMALL에서 shoppingmall_id가 shoppingmall_id인 데이터를 추출해 title을 추출하여 df_travel_visitor_shopping에 추가
+    dbconn = conn.cursor()
+    dbconn.execute(f"SELECT * FROM TB_SHOPPINGMALL WHERE id = {df_travel_visitor_shopping['shoppingmall_id'].values[0]}")
+
+    result = dbconn.fetchall()
+
+    df_shopping = pd.DataFrame(result)
+    df_shopping.columns = [
+        'id',
+        'createdAt',
+        'modifiedAt',
+        'closetime',
+        'location',
+        'opentime',
+        'title',
+        'constituency_id',
+        'user_id',
+    ]
+
+    #title만 df_travel_visitor_shopping에 추가
+    df_travel_visitor_shopping['title'] = df_shopping['title'].values[0]
 
     #TB_TRAVEL_VISITOR_TOUR_LIST에서 travel_id가 travel_id인 데이터를 추출
     dbconn = conn.cursor()
@@ -523,6 +553,28 @@ def find_using_id(travel_id):
     #id를 TRAVEL_TOUR_ID로 변경
     df_travel_visitor_tourist = df_travel_visitor_tourist.rename(columns={'id': 'travel_tourlist_id'})
 
+    #TB_TOUR_LIST에서 tourlist_id가 tourlist_id인 데이터를 추출해 title을 추출하여 df_travel_visitor_tourist에 추가
+    dbconn = conn.cursor()
+    dbconn.execute(f"SELECT * FROM TB_TOURLIST WHERE id = {df_travel_visitor_tourist['tourlist_id'].values[0]}")
+
+    result = dbconn.fetchall()
+
+    df_tourist = pd.DataFrame(result)
+    df_tourist.columns = [
+        'id',
+        'createdAt',
+        'modifiedAt',
+        'closetime',
+        'location',
+        'opentime',
+        'title',
+        'constituency_id',
+        'user_id',
+    ]
+    
+    #title만 df_travel_visitor_tourist에 추가
+    df_travel_visitor_tourist['title'] = df_tourist['title'].values[0]
+
     #TB_TRAVEL_VISITOR_OTHER_SERVICE에서 travel_id가 travel_id인 데이터를 추출
     dbconn = conn.cursor()
     dbconn.execute(f"SELECT * FROM TB_TRAVEL_VISITOR_OTHER_SERVICE WHERE travel_id = {travel_id}")
@@ -551,6 +603,29 @@ def find_using_id(travel_id):
     #id를 TRAVEL_OTHER_ID로 변경
     df_travel_visitor_other = df_travel_visitor_other.rename(columns={'id': 'travel_otherservice_id'})
     
+    #TB_OTHER_SERVICE에서 otherservice_id가 otherservice_id인 데이터를 추출해 title을 추출하여 df_travel_visitor_other에 추가
+    dbconn = conn.cursor()
+    dbconn.execute(f"SELECT * FROM TB_OTHER_SERVICE WHERE id = {df_travel_visitor_other['otherservice_id'].values[0]}")
+
+    result = dbconn.fetchall()
+
+    df_other = pd.DataFrame(result)
+    df_other.columns = [
+        'id',
+        'createdAt',
+        'modifiedAt',
+        'closetime',
+        'location',
+        'opentime',
+        'price',
+        'title',
+        'constituency_id',
+        'user_id',
+    ]
+
+    #title만 df_travel_visitor_other에 추가
+    df_travel_visitor_other['title'] = df_other['title'].values[0]
+
     #df_travel, df_travel_visitor_cafe, df_travel_visitor_restaurant, df_travel_visitor_shopping, df_travel_visitor_tourist, df_travel_visitor_other을 dict로 변환
     travel = df_travel.to_dict(orient='records')
     cafe = df_travel_visitor_cafe.to_dict(orient='records')
@@ -574,11 +649,21 @@ def find_using_id(travel_id):
 def generate_response(msg):
     model = "gpt-3.5-turbo"
     dbconn = conn.cursor()
+    conn.commit()   
 
     response = openai.chat.completions.create(
         model=model,
         messages=[
-            {"role": "system", "content": '너는 사용자가 어느 여행을 하고 싶은지 알아내는 챗봇이야. 1.어느 지역으로 여행을 가고 싶은지, 2. 카페, 식당, 쇼핑, 관광, 기타 중 어느 카테고리로 가고 싶은지 다음 키로 JSON형식으로 제공해 줘:지역-location, 예를 들어, {"location": "서울", "category": "카페"}와 같이 입력해 줘. '},
+            {"role": "system", "content": '''너는 사용자가 어느 여행을 하고 싶은지 알아내는 챗봇이야. 
+            1. 어느 지역으로 여행을 가고 싶은지, 
+            2. 카페, 식당, 쇼핑, 관광, 기타 중 어느 카테고리로 가고 싶은지, 
+            3. 최소 비용 (비용을 명시하지 않으면 0원으로 간주합니다. 또한 비용만 명시해도 0원으로 간주합니다),
+            4. 최대 비용 (비용을 명시하지 않으면 99999999999원으로 간주합니다),
+            5. 최소 인원 (인원을 명시하지 않으면 0명으로 간주합니다. 또한 인원만 명시해도 0명으로 간주합니다),
+            6. 최대 인원 (인원을 명시하지 않으면 50명으로 간주합니다)
+            다음 키로 JSON 형식으로 제공해 줘:
+            {"location": "서울", "category": "카페", "min_expense": 5000, "max_expense": 1500000, "min_serve": 1, "max_serve": 5}
+            '''},
             {"role": "user", "content": msg},
         ],
         temperature=0,
@@ -593,11 +678,18 @@ def generate_response(msg):
 
     #JSON으로 변환된 output_dict에서 location, serving, expense, category를 추출
     location = output_dict['location']
-    #serving = output_dict['serving']
-    #expense = output_dict['expense']
     category = output_dict['category']
+    min_expense = 0
+    max_expense = output_dict['max_expense']
+    min_serve = 0
+    max_serve = output_dict['max_serve']
 
-    print(location, category)
+    print("location : ", location)
+    print("category : ", category)
+    print("min_expense : ", min_expense)
+    print("max_expense : ", max_expense)
+    print("min_serve : ", min_serve)
+    print("max_serve : ", max_serve)
 
     if category == '카페':
         travel_id = cafe_cheap(location)
@@ -634,44 +726,16 @@ def generate_response(msg):
     
     #createAt, modifiedAt, folderName 컬럼을 제거
     df_travel = df_travel.drop(['createdAt', 'modifiedAt', 'folderName'], axis=1)
+
+    #expense가 min_expense보다 크고 max_expense보다 작은 데이터만 추출
+    df_travel = df_travel[(df_travel['travel_expense'] >= min_expense) & (df_travel['travel_expense'] <= max_expense)]
+
+    #serving이 min_serve보다 크고 max_serve보다 작은 데이터만 추출
+    df_travel = df_travel[(df_travel['serving'] >= min_serve) & (df_travel['serving'] <= max_serve)]
     
     travels = df_travel.to_dict(orient='records')
 
-    #TB_AI_CREATE_TRAVEL_LIST를 초기화
-    dbconn.execute(f"DELETE FROM TB_AI_CREATE_TRAVEL_LIST")
-    conn.commit()
-
-
-    #travel_id의 개수만큼 반복
-    for i in travel_id:
-        find_at_travel_id(i)
-
-    conn.commit()
-    
-    dbconn.execute(f"SELECT travel_id, cafe_id, restaurant_id, shoppingmall_id, tourlist_id, otherservice_id FROM TB_AI_CREATE_TRAVEL_LIST")
-    result = dbconn.fetchall()
-    
-    df_travel = pd.DataFrame(result)
-    df_travel.columns = [
-        'travel_id',
-        'cafe_id',
-        'restaurant_id',
-        'shoppingmall_id',
-        'tourlist_id',
-        'otherservice_id',
-    ]
-    
-    print(df_travel)
-    
-    #travel_id가 같은 데이터끼리 리스트로 묶음
-    df_travel = df_travel.groupby('travel_id').agg(lambda x: x.tolist()).reset_index()
-    
-    #각 리스트에서 nan을 제거
-    df_travel = df_travel.applymap(lambda x: [i for i in x if str(i) != 'nan'] if isinstance(x, list) else x)
-    
-    print(df_travel)
-    
-    #df_travel을 json으로 변환
-    df_travel_json = df_travel.to_json(orient='records')
+    #travels를 10개 추출
+    travels = travels[:10]
 
     return travels
