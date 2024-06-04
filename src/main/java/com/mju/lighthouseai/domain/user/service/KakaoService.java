1db4a30d26c08b6fbb2feab6d61a25975a7bfc98
Euna Jeong <104022743+eunaJ@users.noreply.github.com>
2024-06-04 13:28:52 +0900

    refactor: kakao 로그인 프로필 이미지 널 처리

--- src/main/java/com/mju/lighthouseai/domain/user/service/KakaoService.java
+++ src/main/java/com/mju/lighthouseai/domain/user/service/KakaoService.java
@@ -110,6 +110,13 @@ public class KakaoService {
 //                +jsonNode.get("kakao_account").get("birthday").asText().substring(2);
         String birth = "";
         String profile_img_url = jsonNode.get("kakao_account").get("profile").get("profile_image_url").asText();
+        String profile_img_url;
+        if(jsonNode.get("kakao_account").get("profile").get("profile_image_url") != null){
+            profile_img_url = jsonNode.get("kakao_account").get("profile").get("profile_image_url").asText();
+        }
+        else {
+            profile_img_url = "";
+        }
         return new KakaoUserInfoDto(email, nickname, birth, profile_img_url);
     }
 
